8be1b8b8355416844ec9d47259f995018110146e
Asilbek <asiloke797@gmail.com>
2025-12-07 22:27:52 +0500

    add route

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import register, StudentList
 
 urlpatterns = [
-    path('', register, name='register'),
+    path('register/', register, name='register'),
     path('list/', StudentList.as_view()),
 ]
